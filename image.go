4a9f13698abb9f8f9b6b3f099c0b8e9e9236aad5
Cédric Connes <cedric.connes@gmail.com>
2020-05-19 14:40:43 +0200

    Improve icon image decoding

--- image.go
+++ image.go
@@ -11,18 +11,12 @@ import (
 var five2eight [1 << 5]uint8
 var six2eight [1 << 6]uint8
 
-var iconBlock [1 << 6]int
-
 func init() {
 	for i := range five2eight {
 		five2eight[i] = uint8(math.Round(float64(i) * 255.0 / 31.0))
 	}
 	for i := range six2eight {
 		six2eight[i] = uint8(math.Round(float64(i) * 255.0 / 63.0))
-	}
-	for i := range iconBlock {
-		// Swap bits: 5 4 3 2 1 0 => 5 3 1 4 2 0
-		iconBlock[i] = i&33 | (i&16)>>2 | (i&8)<<1 | (i&4)>>1 | (i&2)<<2
 	}
 }
 
@@ -45,11 +39,12 @@ func DecodeIconImage(src []byte, width int) (image.Image, error) {
 	for i := 0; i < pixels; i++ {
 		pixel := binary.LittleEndian.Uint16(src[2*i:])
 
-		block := i / 64
-		blockIndex := iconBlock[i%64]
+		block := i >> 6                           // bits >= 6
+		blockX := (i&16)>>2 | (i&4)>>1 | i&1      // bits 4 2 0
+		blockY := (i&32)>>3 | (i&8)>>2 | (i&2)>>1 // bits 5 3 1
 
-		x := 8*(block%widthBlocks) + blockIndex%8
-		y := 8*(block/widthBlocks) + blockIndex/8
+		x := (block%widthBlocks)<<3 | blockX
+		y := (block/widthBlocks)<<3 | blockY
 		c := color.NRGBA{
 			R: five2eight[pixel>>11],
 			G: six2eight[(pixel>>5)&0x3f],
